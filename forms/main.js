6b42d1f03a0bc5ad7c2faa85a7e7a7c18ae1c376
GS-Manoj-Kumar <147612752+GS-Manoj-Kumar@users.noreply.github.com>
2024-07-02 17:03:37 +0530

    swiper uninstall
    swiper was causing some wired issue

--- forms/main.js
+++ forms/main.js
@@ -2,25 +2,4 @@
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
-import './node_modules/swiper/swiper.css';
-import './node_modules/swiper/modules/navigation.css';
-import './node_modules/swiper/modules/pagination.css';
-import Swiper, { Navigation, Pagination } from 'swiper';
 
-Swiper.use([Navigation, Pagination]);
-
-document.addEventListener('DOMContentLoaded', () => {
-  const swiper = new Swiper('.swiper-container', {
-    direction: 'horizontal',
-    loop: false,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    allowTouchMove: false,
-  });
-});
